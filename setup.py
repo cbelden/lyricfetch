06289ab9fbe564bae02505562e458f913b1a1dfe
Calvin Belden <cbelden@nd.edu>
2014-05-28 10:18:11 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='LyricFetch',
+      version='1.0',
+      description='Lyric Fetcher Tool',
+      author='Cal Belden',
+      author_email='calvinbelden@gmail.com',
+      packages=['lyricfetch'],
+      requires=['beautifulsoup4==4.3.2',
+                'requests==2.3.0',
+                'wsgiref==0.1.2'])
